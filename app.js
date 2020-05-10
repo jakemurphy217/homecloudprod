bbcc0435927067b722cf4d59e08e72ad10b5334d
jakemurphy217 <jakemurphy217@gmail.com>
2020-05-10 11:08:55 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -36,23 +36,23 @@ app.use("/uploads", express.static(path.join(__dirname,"uploads")));
 app.use("/", express.static(path.join(__dirname,"angular")));
 
 
-// app.use((req, res, next) => {
-//
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//
-//   res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
-//
-//   res.setHeader("Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept,Authorization"
-//   );
-//
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//
-//
-//   next();
-//
-//
-// });
+app.use((req, res, next) => {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+
+  res.setHeader("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+  );
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+  next();
+
+
+});
 
 
 app.use("/api/posts", postsRoutes);
