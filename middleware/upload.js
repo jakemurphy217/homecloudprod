4a9e92d41046871e7eb5e8ba7566c81d9d5356a7
jakemurphy217 <jakemurphy217@gmail.com>
2020-05-10 10:54:19 +0100

    Add files via upload

--- middleware/upload.js
+++ middleware/upload.js
@@ -34,7 +34,7 @@ const storage = multer.diskStorage({
     // if (isValid) {
     //   error = null;
     // }
-    cb(null, 'backend/uploads')
+    cb(null, "uploads");
   },
   filename: (req, file, cb) => {
     const name = file.originalname.toLowerCase().split(' ').join('-');
