dee5253d31dfe0114fe267b64f8cc477d6373314
jakemurphy217 <jakemurphy217@gmail.com>
2020-05-10 01:19:03 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');
+
 
 const postsRoutes = require('./routes/posts');
 const userRoutes = require('./routes/user');
